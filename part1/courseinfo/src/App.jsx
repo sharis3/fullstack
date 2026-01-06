65eac4953cb41e17dbb93b94fe3c8cd19c2cab05
sharis3 <sharis@hawk.iit.edu>
2026-01-05 19:58:13 -0600

    part 1.3: course parts as objects

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,32 +1,38 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
+        part1={part1} 
+        part2={part2}
+        part3={part3} 
       />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
 
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => (
+const Content = ({ part1, part2 ,part3}) => (
   <>
-    <Part name={part1} exerciseCount={exercises1}/>
-    <Part name={part2} exerciseCount={exercises2}/>
-    <Part name={part3} exerciseCount={exercises3}/>
+    <Part part = {part1}/>
+   <Part part = {part2}/>
+     <Part part = {part3}/>
   </>
 )
 
@@ -34,11 +40,8 @@ const Total = ({ exercises1, exercises2, exercises3 }) => (
   <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 )
 
-const Part = ({name, exerciseCount}) => {
-  return(
-    <p>{name} {exerciseCount}</p>
-  )
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
   
-}
+
 
 export default App
